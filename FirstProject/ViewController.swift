787240db0886100b0ccdff5a67000b6a7ccc4203
sgiorgishvili <sgiorgishvili@tbcbank.com.ge>
2022-07-01 21:53:54 +0400

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by TBC on 01.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
